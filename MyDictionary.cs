0c2a58eb8c50a74417df9c78a34aba31074376e9
mervenur <mervenur.karadas1@gmail.com>
2021-02-28 23:27:16 +0300

    Proje dosyası ekle.

---
+++ MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4GunOdev5
+{
+    class MyDictionary<Tkey, Tvalue> 
+    {
+        Tkey[] person;
+        Tvalue[] ıd;
+
+        public MyDictionary() //Constructor
+        {
+            person = new Tkey[0];  
+            ıd = new Tvalue[0];
+        }
+
+        public void Add(Tkey persons, Tvalue ıds)
+        {
+            Tkey[] tempArray1 = person;
+            Tvalue[] tempArray2 = ıd;
+
+            person = new Tkey[person.Length + 1];
+            ıd = new Tvalue[ıd.Length + 1];
+
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                person[i] = tempArray1[i];
+            }
+            person[person.Length - 1] = persons;
+
+
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                ıd[i] = tempArray2[i];
+            }
+            ıd[ıd.Length - 1] = ıds;
+
+            Console.WriteLine("Person = " + persons + "\nID = " + ıds);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4GunOdev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Ali", 1);
+            myDictionary.Add("Ayşe", 2);
+            myDictionary.Add("Rıza", 3);
+            myDictionary.Add("Hikmet", 4);
+
+        }
+    }
+}
